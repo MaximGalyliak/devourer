0223669794c2769fdb69bb9819d859df2bde419e
Adler11th <mgoftest@gmail.com>
2018-05-01 23:33:11 -0700

    added connection statement

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 const mySQL = require('mysql');
-const connection = mySQL.createConnection({
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: 'root',
-    database: 'things_db'
-});
+
+var connection;
+if(process.env.JAWSDB_URL){
+   connection = mySQL.createConnection(process.env.JAWSDB_URL);
+}else{
+   connection = mySQL.createConnection({
+      host: '127.0.0.1',
+      port: '3306',
+      user: 'root',
+      password: 'root',
+      database: 'things_db'
+  });
+}
+
 //test connection
 connection.connect((error)=>{
     if(error){
